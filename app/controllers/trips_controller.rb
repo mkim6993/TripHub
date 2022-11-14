fab573b8b3467afa2cc899c055e286570c4041e5
minsung <kmaxsung@gmail.com>
2022-11-13 21:39:52 -0500

    adding new locations from trip, location-trip relationship, search locations

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,6 +19,34 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  # add a location from newly created trip
+  def locations
+    @trip = Trip.find(params[:id]) 
+    @location = Location.new
+  end
+
+  # post method when new location is created from /trips/:id/locations
+  def create_location
+    trip_id = location_params["trip_id"]
+    modified_location_params = location_params
+    modified_location_params.extract!(:trip_id)
+
+    puts "^^^^^^^^^^^^^^^^^"
+    @location = Location.new(modified_location_params)
+    puts "^^^^^^^^^^^^^^^^^"
+
+    respond_to do |format|
+      if @location.save
+        TripLocation.create(trip_id: trip_id, location_id: @location.id)
+        format.html { redirect_to "/trips/" << trip_id.to_s, notice: "Location was successfully created." }
+        format.json { render :show, status: :created, location: @location }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # GET /trips/1/edit
   def edit
   end
@@ -29,7 +57,7 @@ class TripsController < ApplicationController
 
     respond_to do |format|
       if @trip.save
-        format.html { redirect_to new_location_path(trip_id: @trip.id), notice: "Trip was successfully created." }
+        format.html { redirect_to trip_url(@trip), notice: "Trip was successfully created." }
         format.json { render :show, status: :created, location: @trip }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -55,8 +83,8 @@ class TripsController < ApplicationController
 
   # DELETE /trips/1 or /trips/1.json
   def destroy
+    TripLocation.where(trip_id: @trip.id).destroy_all # destroy all TripLocations relationships related to trip
     @trip.destroy
-
     respond_to do |format|
       format.html { redirect_to trips_url, notice: "Trip was successfully destroyed." }
       format.json { head :no_content }
@@ -73,4 +101,10 @@ class TripsController < ApplicationController
     def trip_params
       params.require(:trip).permit(:create_by, :trip_date, :title, :description, :upvotes, :shares, :public, :parent)
     end
+
+    # Only allow a list of trusted parameters through.
+    def location_params
+      params.require(:location).permit(:trip_id, :name, :description, :address, :contact, :price, :image, :open_times, images: [])
+    end
 end
+
