a1ec9adf519b68a6a9fd4b0d97e77e77b52f1e61
Cole <coledfeuer@gmail.com>
2022-12-03 17:05:00 -0500

    small carousel changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
-og_id = Trip.create(create_by: "CF", trip_date: DateTime.now, title: "socrates", description: "why?", upvotes: 1, shares: 0, public: true, parent:-1).id
-child1 = Trip.create(create_by: "MSK", trip_date: DateTime.now, title: "confusious", description: "how?", upvotes: 1, shares: 0, public: true, parent:og_id).id
-child2 = Trip.create(create_by: "JT", trip_date: DateTime.now, title: "decarte", description: "what?", upvotes: 1, shares: 0, public: true, parent:og_id).id
-gchild1 = Trip.create(create_by: "CF", trip_date: DateTime.now, title: "Roe Jogan", description: "cringe?", upvotes: 1, shares: 0, public: true, parent:child1).id
-gchild2 = Trip.create(create_by: "MSK", trip_date: DateTime.now, title: "Peter", description: "lois?", upvotes: 1, shares: 0, public: true, parent:child1).id
-gchild3 = Trip.create(create_by: "JT", trip_date: DateTime.now, title: "post", description: "kek?", upvotes: 1, shares: 0, public: true, parent:child2).id
-gchild4 = Trip.create(create_by: "HC", trip_date: DateTime.now, title: "momma", description: "jo?", upvotes: 1, shares: 0, public: true, parent:child2).id
-gchild5 = Trip.create(create_by: "CF", trip_date: DateTime.now, title: "no", description: "u?", upvotes: 1, shares: 0, public: true, parent:child2).id
-gchild6 = Trip.create(create_by: "MSK", trip_date: DateTime.now, title: "trip", description: "hub?", upvotes: 1, shares: 0, public: true, parent:gchild4).id
+og_id = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "socrates", description: "why?", upvotes: 1, shares: 0, public: true, parent:-1).id
+child1 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "confusious", description: "how?", upvotes: 1, shares: 0, public: true, parent:og_id).id
+child2 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "decarte", description: "what?", upvotes: 1, shares: 0, public: true, parent:og_id).id
+gchild1 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "Roe Jogan", description: "cringe?", upvotes: 1, shares: 0, public: true, parent:child1).id
+gchild2 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "Peter", description: "lois?", upvotes: 1, shares: 0, public: true, parent:child1).id
+gchild3 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "post", description: "kek?", upvotes: 1, shares: 0, public: true, parent:child2).id
+gchild4 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "momma", description: "jo?", upvotes: 1, shares: 0, public: true, parent:child2).id
+gchild5 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "no", description: "u?", upvotes: 1, shares: 0, public: true, parent:child2).id
+gchild6 = Trip.create(create_by: "CDFire", trip_date: DateTime.now, title: "trip", description: "hub?", upvotes: 1, shares: 0, public: true, parent:gchild4).id
+
+TripUser.create(user_id: 1, trip_id: 1)
+TripUser.create(user_id: 1, trip_id: 2)
+TripUser.create(user_id: 1, trip_id: 3)
+TripUser.create(user_id: 1, trip_id: 4)
+TripUser.create(user_id: 1, trip_id: 5)
+TripUser.create(user_id: 1, trip_id: 6)
+TripUser.create(user_id: 1, trip_id: 7)
+TripUser.create(user_id: 1, trip_id: 8)
+TripUser.create(user_id: 1, trip_id: 9)
 
 puts og_id
 # 10.times do
