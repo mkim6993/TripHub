3202e831c2de7e61247996ec92f44817cb9b2f2f
Cole <coledfeuer@gmail.com>
2022-12-03 18:12:54 -0500

    get most popular children function

---
+++ app/helpers/merge_helper.rb
@@ -0,0 +1,15 @@
+module MergeHelper
+    def getMostPopularRelated(trip)  
+        all_related = get_all_children(trip.id, []).append(trip)
+        all_related.sort_by{|t| -1*get_likes(trip)}.first(5)
+    end
+        
+    def get_all_children(trip_id, child_arr)
+        children = Trip.where(parent: trip_id)
+        while children.length != 0
+            child_arr += children
+            children = Trip.where(parent: children.ids)
+        end
+        child_arr
+    end
+end
---
+++ app/javascript/custom/mergeTrips.js
@@ -0,0 +1,64 @@
+document.addEventListener("turbo:load", function () {
+    let popBtn = document.querySelector("#merge");
+    if(popBtn){
+        popBtn.addEventListener("click", function (event) {
+            event.stopPropagation();
+            popup();
+        });
+    }
+});
+
+const getData = (searchInput) => {
+    return $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/search_user",
+        data: { search: searchInput },
+        success: function (data) {
+            console.log(data);
+            console.log("success");
+        },
+    });
+};
+
+const popup = async () => {
+    $('#mergeScreen').css({ opacity: 0.7, 'width':$(document).width(),'height':$(document).height()});
+
+    var userSearch = document.getElementById("mergeWindow")
+    userSearch.style.display = 'block';
+}
+
+const showSearch = async () => {
+
+    document.getElementById("userResults").innerHTML = "";
+    var htmlString =
+        "<div id='noResult'>No results. Create a new location?</div>";
+    const userSearch = document.getElementById("userSearch").value;
+    if (userSearch == "") {
+        alert("no search");
+    } else {
+        try {
+            var users = await getData(userSearch);
+            console.log("here", users);
+            if (users.length != 0) {
+                htmlString = "";
+                for (let i = 0; i < users.length; i++) {
+                    htmlString +=
+                        "<div class='userItem'><div class='usrName'>" +
+                        users[i].name +
+                        "</div><div class='usrDetailAndImage'><div class='usrDetails'><div class='detailText'>" +
+                        users[i].username +
+                        "</div><div class='detailText'>" +
+                        users[i].email +
+                        "</div></div></div></div>";
+                }
+            }
+            document.getElementById("userResults").innerHTML += htmlString;
+            let userResults = document.querySelector("#userResults");
+            userResults.style.maxHeight = userResults.scrollHeight + "px";
+        } catch (err) {
+            console.log("there was an error when fetching data", err);
+            document.getElementById("searchResults").innerHTML += htmlString;
+        }
+    }
+};
